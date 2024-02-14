9b97f77a194b063c6d39a3ce4b2496bb51b24c0e
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-02-14 17:10:46 +0000

    comments section added to user photo show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get("/users/:path_username", { :controller => "users", :action => "show" })
 
   get("/photos", { :controller => "photos", :action => "index" })
+
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
 end
